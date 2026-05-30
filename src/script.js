f3f65c99f19d1ffdeecebe479eb0b3dd7165490e
Saulo de Freitas <97735487+SauloDeFreitas@users.noreply.github.com>
2026-05-29 21:55:38 -0300

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -4,13 +4,11 @@ function enviarMensagem() {
 
     // Pegar os campos para montar mensagem
     const mensagem = document.getElementById("mensagem")
-    const categoria = document.getElementById("categoria")
     const nome = document.getElementById("nome")
 
     if (formulario.checkValidity()) {
         mensagem_completa.value = `
-                                    Meu nome é ${nome.value},
-                                    estou entrando em contado pois preciso de ${categoria.value}.
+                                    Meu nome é ${nome.value}, gostaria de saber mais sobre:
                                     *Mais informações:* ${mensagem.value}
                                     `
         formulario.submit()
